ef2774526882cb9d2820a9c16a8998a58bc2e736
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2021-12-01 16:51:39 +0100

    [update] banniere.js -> have a download cv button from entete/cv.js

--- src/components/Entete/Banniere.js
+++ src/components/Entete/Banniere.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Image from '../../assets/img/AV.png';
 import Assets from '../../assets/img/pc.png';
+import Cv from './Cv';
 
 function Banniere() {
     return (
         <div>
             <div className="d-flex justify-content-center">
-                <img className="avatar" src={Image} alt="Avatar should be here"/>
+                <img className="avatar" src={Image} alt="Avatar should be here" />
             </div>
-            <div  className="text-center mt-5" >
+            <div  className="text-center mt-5 h3" >
                 <p style={{ fontStyle: 'italic' }}>"La liberté c'est de savoir danser avec ses chaines" F. Nietzsche</p>
             </div>
+            <Cv />
             <div className="d-flex justify-content-center">
                 <img className="pc" src={Assets} alt="pc" />
             </div>

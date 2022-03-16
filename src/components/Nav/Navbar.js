da55a5fda1e3825f8779e91feab1203b421154e5
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2022-03-16 14:00:49 +0100

    [udpate] nav -> fond de la nav transparent

--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -5,8 +5,8 @@ import { Link } from 'react-scroll';
 function Navbar() {
     return (
         <div>
-            <nav className="fixedElement d-flex justify-content-end mr-3">
-                <Link to="contact" smooth={true}><button className="btn btn-lg btn-outline-secondary m-3">Say Hello</button></Link>   
+            <nav className="fixedElement d-flex justify-content-end mr-3 bg-transparent">
+                <Link to="contact" smooth={true}><button className="btn btn-lg btn-outline-secondary m-3 bg-white">Say Hello</button></Link>   
             </nav>
         </div>
     )

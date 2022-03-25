9a6a77ceea0d40c41710e8fd2effab746c317c11
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2022-03-25 18:49:18 +0100

    [add] stacke -> nouveaux logos

--- src/components/Body/Stacks/Stacks.js
+++ src/components/Body/Stacks/Stacks.js
@@ -4,17 +4,17 @@ import Symfony from '../../../assets/img/symfony.png'
 import Mysql from '../../../assets/img/mysql.png';
 import Html from '../../../assets/img/html.png';
 import Jquery from '../../../assets/img/jquery.png';
-import Laravel from '../../../assets/img/laravel.png';
 import Node from '../../../assets/img/node.png';
 import ReactLogo from '../../../assets/img/react.png';
 import Github from '../../../assets/img/github.png';
+import Tailwind from "../../../assets/img/tailwindcss.png"
+import Mongo from "../../../assets/img/mongodb.png"
 
 function Stacks() {
     return (
         <div className="container stacks">
             <h2 className="pt-5">Technologies utilisées:</h2>
             <img className="icon" src={Symfony} alt="symfony" />
-            <img className="icon" src={Laravel} alt="laravel" />
             <img style={{ height: 90, width: 140, margin: 30 }} src={Node} alt="Node" />
             <img className="icon" src={ReactLogo} alt="ReactLogo" />
             <img className="icon" src={Php} alt="Php" />
@@ -22,7 +22,8 @@ function Stacks() {
             <img className="icon" src={Html} alt="Html" />
             <img className="icon" src={Jquery} alt="Jquery" />
             <img className="icon" src={Github} alt="Github" />
-            
+            <img className="icon" src={Tailwind} alt="Tailwind" />
+            <img className="icon" src={Mongo} alt="Mongo" />            
         </div>
     )
 }

1dfa42c46fca98ae167d0d151c8977aab8f8c16b
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2022-03-28 17:09:22 +0200

    [update] implémentation et ajout de maquette.js sur projet.js

--- src/components/Body/Projets/Maquette/Maquette.js
+++ src/components/Body/Projets/Maquette/Maquette.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import ImageGallery from 'react-image-gallery';
+import Home from "../../../../assets/img/maquette.jpg"
+import Keyboard from "../../../../assets/img/keyboard.jpg"
+import Mouse from "../../../../assets/img/mouse.jpg"
+import Computers from "../../../../assets/img/computers.jpg"
 
 const Maquette = () => {
+
+  const images = [
+    {
+      original: `${Home}`,
+      thumbnail: `${Home}`,
+    },
+    {
+      original: `${Computers}`,
+      thumbnail: `${Computers}`,
+    },
+    {
+      original: `${Keyboard}`,
+      thumbnail: `${Keyboard}`,
+    },
+    {
+      original: `${Mouse}`,
+      thumbnail:`${Mouse}`,
+    },
+  ];
+
   return (
-    <div>Maquette</div>
+    <div className="caroussel" >
+      <ImageGallery items={images} showNav={false} showPlayButton={false} />
+    </div>
   )
 }
 
--- src/components/Body/Projets/Projets.js
+++ src/components/Body/Projets/Projets.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'react'
+import React, { useState } from 'react'
 import Pinterest from './Pinterest/Pinterest'
 import Twitter from './Twitter/Twitter'
 import Snapchat from './Snapchat/Snapchat'
@@ -10,6 +10,7 @@ import ModalSnapchat from './Snapchat/ModalSnapchat';
 import ModalTwitter from './Twitter/ModalTwitter';
 import Prevon from './Prev_On/Prevon'
 import ModalPrevon from "./Prev_On/ModalPrevon"
+import MaquetteDesign from './Maquette/Maquette.js'
 
 function Projets() {
     const [isOpenPinterest, setIsOpenPinterest] = useState(false);
@@ -20,60 +21,88 @@ function Projets() {
 
     return (
         <>
-                <h2 className="text-center pb-3">Mes Projets</h2>
-                <div className="row">
-                    <div className="col-sm-6">
-                        <div className="card mb-4" onClick={() => setIsOpenPinterest(true)}>
-                            <div className="card-body" id="pinterest">
-                                <CardActionArea>
-                                    <Pinterest />
-                                </CardActionArea>
-                            </div>
+            <h2 className="text-center p-3">Mes Projets</h2>
+            <div className="row">
+                <div className="col-sm-6">
+                    <div className="card mb-4">
+                        <div className="card-body" id="caroussel">
+                            <CardActionArea>
+                                <MaquetteDesign />
+                            </CardActionArea>
                         </div>
-                        {isOpenPinterest && <ModalPinterest setIsOpenPinterest={setIsOpenPinterest} />}
-                    </div>
-                    <div className="col-sm-6">
-                        <div className="card mb-4" onClick={() => setIsOpenPrevon(true)}>
-                            <div className="card-body" id="prevon">
-                                <CardActionArea>
-                                    <Prevon />
-                                </CardActionArea>
-                            </div>
-                        </div>
-                        {isOpenPrevon && <ModalPrevon setIsOpenPrevon={setIsOpenPrevon} />}
-                    </div>
-                    <div className="col-sm-6">
-                        <div className="card mb-4"  onClick={() => setIsOpenTwitter(true)}>
-                            <div className="card-body" id="twitter">
-                                <CardActionArea>
-                                    <Twitter />
-                                </CardActionArea>
-                            </div>
-                        </div>
-                        {isOpenTwitter && <ModalTwitter setIsOpenTwitter={setIsOpenTwitter} />}
-                    </div>
-                    <div className="col-sm-6">
-                        <div className="card mb-4"  onClick={() => setIsOpenSnapchat(true)}>
-                            <div className="card-body" id="snpachat">
-                                <CardActionArea>
-                                    <Snapchat />
-                                </CardActionArea>
-                            </div>
-                        </div>
-                        {isOpenSnapchat && <ModalSnapchat setIsOpenSnapchat={setIsOpenSnapchat} />}
-                    </div>
-                    <div className="col-sm-6">
-                        <div className="card mb-4" onClick={() => setIsOpenCinema(true)}>
-                            <div className="card-body" id="mycinema">
-                                {/* <h5 class="card-title text-center">PHP Natif</h5> */}
-                                <CardActionArea>
-                                    <MyCinema />
-                                </CardActionArea>
-                            </div>
-                        </div>
-                        {isOpenCinema && <ModalCinema setIsOpenCinema={setIsOpenCinema} />}
                     </div>
                 </div>
+                <div className="col-sm-6">
+                    <div className="card mb-4 border-0">
+                        <div className="card-body text-dark text-justify lh-lg fs-5">
+                            <p>Voici une maquette que j'ai réalisé dans le cadre de ma formation;
+                                il s'agissait de réaliser le prototype d'un site fictif nommé Omega Gaming.
+                                Il fallait donc respecter le cahier des charges qui imposait certains éléments
+                                (dont la colométrie ...) mais le reste était libre.
+                                Pour le descriptif du projet entier, veuillez suivre ce lien <a href="https://github.com/dounyadelren/maquette_design" target="_blank" rel="noreferrer">github</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <p className="text-dark text-justify lh-lg fs-5 mt-5">J'ai publié ci-dessous quelques uns de mes travaux réalisés en formation
+                ou de manière personnelle. Ce sont des vidéos-hover (comme sur Youtube, elles se jouent
+                lorsque la souris passe dessus), vous aurez le descriptif ainsi que le lien du répertoire
+                en cliquant sur l'image!
+            </p>
+            <div className="row">
+                <div className="col-sm-6">
+                    <div className="card mb-4" onClick={() => setIsOpenPrevon(true)}>
+                        <div className="card-body" id="prevon">
+                            <CardActionArea>
+                                <Prevon />
+                            </CardActionArea>
+                        </div>
+                    </div>
+                    {isOpenPrevon && <ModalPrevon setIsOpenPrevon={setIsOpenPrevon} />}
+                </div>
+                <div className="col-sm-6">
+                    <div className="card mb-4" onClick={() => setIsOpenPinterest(true)}>
+                        <div className="card-body" id="pinterest">
+                            <CardActionArea>
+                                <Pinterest />
+                            </CardActionArea>
+                        </div>
+                    </div>
+                    {isOpenPinterest && <ModalPinterest setIsOpenPinterest={setIsOpenPinterest} />}
+                </div>
+                <div className="col-sm-6">
+                    <div className="card mb-4" onClick={() => setIsOpenTwitter(true)}>
+                        <div className="card-body" id="twitter">
+                            <CardActionArea>
+                                <Twitter />
+                            </CardActionArea>
+                        </div>
+                    </div>
+                    {isOpenTwitter && <ModalTwitter setIsOpenTwitter={setIsOpenTwitter} />}
+                </div>
+                <div className="col-sm-6">
+                    <div className="card mb-4" onClick={() => setIsOpenSnapchat(true)}>
+                        <div className="card-body" id="snpachat">
+                            <CardActionArea>
+                                <Snapchat />
+                            </CardActionArea>
+                        </div>
+                    </div>
+                    {isOpenSnapchat && <ModalSnapchat setIsOpenSnapchat={setIsOpenSnapchat} />}
+                </div>
+                <div className="col-sm-6">
+                    <div className="card mb-4" onClick={() => setIsOpenCinema(true)}>
+                        <div className="card-body" id="mycinema">
+                            {/* <h5 class="card-title text-center">PHP Natif</h5> */}
+                            <CardActionArea>
+                                <MyCinema />
+                            </CardActionArea>
+                        </div>
+                    </div>
+                    {isOpenCinema && <ModalCinema setIsOpenCinema={setIsOpenCinema} />}
+                </div>
+            </div>
         </>
     )
 }
